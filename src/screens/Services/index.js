ccc5ee9684886d61b607ad48c2d542b70af2d5d9
lazaroconceicao <joselazaroc@gmail.com>
2023-03-21 21:12:42 -0300

    First screen

---
+++ src/screens/Services/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { SafeAreaView, StatusBar, Text } from "react-native";
+
+export default function Services() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Serviços!</Text>
+    </SafeAreaView>
+  );
+}
